20e332e75d8e3542347c138eb15261ad4d70f2a0
Dev Patel <devpatel2556@gmail.com>
2025-01-27 14:17:57 -0500

    get key/value

--- project/src/canvas_layer.gd
+++ project/src/canvas_layer.gd
@@ -3,11 +3,15 @@ extends CanvasLayer
 
 var keyInput: LineEdit
 var valueInput: LineEdit
+var getValueInput: LineEdit
+var outputLabel: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	keyInput = $TextureRect/keyinput
 	valueInput = $TextureRect/valueinput
+	getValueInput = $TextureRect/getvalueinput
+	outputLabel = $TextureRect/ColorRect/outputlabel
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,3 +26,19 @@ func _on_button_pressed() -> void:
 		JavaScriptBridge.eval(completeString)
 	else:
 		print("The JavaScriptBridge singleton is NOT available")
+
+
+func _on_button_2_pressed() -> void:
+	var jsString = "localstorage.getItem(%s)"
+	var completeString = jsString % [getValueInput.text]
+	
+	var result
+	if OS.has_feature('web'):
+		var jsResult = JavaScriptBridge.eval(completeString)
+		if (jsResult): result = jsResult
+		else: result = "Could not find key"
+	else:
+		result = "Could not get localstorage"
+	
+	outputLabel.text = result
+	
